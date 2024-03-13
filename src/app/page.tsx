de293ffd68c914398bae61188183248056f117b3
Fida ur Rahman <fidaurr900@gmail.com>
2024-03-13 23:51:45 +0500

    Navbar and Herosection are added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-import Image from "next/image";
+
+// import FeaturedCourses from "@/components/FeaturedCourses";
+// import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+// import Instructors from "@/components/Instructors";
+// import MusicSchoolTestimonials from "@/components/TestimonialCards";
+// import UpcomingWebinars from "@/components/UpcomingWebinars";
+// import WhyChooseUs from "@/components/WhyChooseUs";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1 className="text-4xl font-bold mb-8">Welcome to Next.js</h1>
-        <p className="text-lg">Get started by editing{" "}</p>
-      </div>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
     </main>
   );
 }
