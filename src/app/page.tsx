8db6b100369cacf94f05614ed923ed3e3b19a21b
Fida ur Rahman <fidaurr900@gmail.com>
2024-03-14 00:11:13 +0500

    Cards are added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 
-// import FeaturedCourses from "@/components/FeaturedCourses";
+import FeaturedCourses from "@/components/FeaturedCourses";
 // import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 // import Instructors from "@/components/Instructors";
@@ -12,6 +12,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
+      <FeaturedCourses />
     </main>
   );
 }
