1c1662ac08c597da7a24a44d2fa377aafaa3de5a
Fida ur Rahman <fidaurr900@gmail.com>
2024-03-14 00:55:49 +0500

    all the components are added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 
 import FeaturedCourses from "@/components/FeaturedCourses";
-// import Footer from "@/components/Footer";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 // import Instructors from "@/components/Instructors";
 // import MusicSchoolTestimonials from "@/components/TestimonialCards";
 // import UpcomingWebinars from "@/components/UpcomingWebinars";
-// import WhyChooseUs from "@/components/WhyChooseUs";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
@@ -13,6 +13,8 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedCourses />
+      <WhyChooseUs />
+      <Footer />
     </main>
   );
 }
